3882ea5c4bf299f7c18940575e15a007e0353fb7
Borek Bandell <borekbandell@hotmail.com>
2024-04-18 15:15:59 +0200

    Added queue get-all-videos

--- routes/VideoRouterRabbit.ts
+++ routes/VideoRouterRabbit.ts
@@ -1,13 +1,27 @@
 import { Connection } from 'rabbitmq-client';
+import { IVideoService } from '../services/IVideoService';
 
 export class VideoRouterRabbit {
     private rabbit: Connection;
+    private videoService: IVideoService;
 
-    constructor(rabbit: Connection) {
+    constructor(rabbit: Connection, videoService: IVideoService) {
         this.rabbit = rabbit;
+        this.videoService = videoService;
     }
 
     public start(): void {
+        const getAllVideosServer = this.rabbit.createConsumer(
+            {
+                queue: 'get-all-videos',
+            },
+            async (req, reply) => {
+                console.log('Get all videos request:', req.body);
+                const videos = await this.videoService.getAllVisibleVideos();
+                reply(videos);
+            }
+        );
+
         const createVideoServer = this.rabbit.createConsumer(
             {
                 queue: 'create-video',
@@ -40,6 +54,7 @@ export class VideoRouterRabbit {
 
         process.on('SIGINT', async () => {
             await Promise.all([
+                getAllVideosServer.close(),
                 createVideoServer.close(),
                 updateVideoServer.close(),
                 deleteVideoServer.close(),
