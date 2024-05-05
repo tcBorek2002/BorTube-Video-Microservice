5161c631f9e5505c7de1c7f1e58c5ca24ea8899a
Borek Bandell <borekbandell@hotmail.com>
2024-05-05 20:23:13 +0200

    Added migration - users added

---
+++ prisma/migrations/20240505182233_added_users/migration.sql
@@ -0,0 +1,18 @@
+-- AlterTable
+ALTER TABLE "Video" ADD COLUMN     "userId" TEXT;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "displayName" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Video" ADD CONSTRAINT "Video_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
